759848750addb76bb188c42204348f678e3faf7e
Peaceultime <clem31470@gmail.com>
2019-12-18 16:50:27 +0100

    Add changelog CSS and HTML structure

--- src/lib/frontend/ChangelogPopup.js
+++ src/lib/frontend/ChangelogPopup.js
@@ -9,7 +9,26 @@ module.exports = class ChangelogPopup extends Popup
 	{
 		super(askTranslation("changelog-popup"));
 
+		/*
 		
+		<div class="changelog-content">
+			<div class="log-group log-hotfix">
+				<div class="log-version">Version 1.1.1 - Hotfix</div>
+				<div class="log-list">
+					<div class="log">Fix an Internet connection bug</div>
+				</div>
+			</div>
+			<div class="log-group">
+				<div class="log-version">Version 1.1.0</div>
+				<div class="log-list">
+					<div class="log">Add changelog</div>
+					<div class="log">Add setting to skip patch update furing start</div>
+					<div class="log">Add button in patch list to update patches</div>
+				</div>
+			</div>
+		</div>
+		
+		*/
 
 		this.content = Utils.nodes.children(Utils.nodes.div("changelog-content"), []);
 	}
